f32030f2f8499bd9d50aa6cde77ff03f007d9d64
Churchill Yik <476418125@qq.com>
2012-09-10 18:11:23 +0800

    working at sina login

--- MJTool/Login.cs
+++ MJTool/Login.cs
@@ -42,7 +42,7 @@ namespace MJTool
 			Match m = Regex.Match(result, "<div class=\"box\">\\s*?<a\\s*?href=\"http://weibo\\.com/(.*?)\"\\s*?class=\"btn-login\">登录微博</a>", RegexOptions.Singleline);
 			if (!m.Success)
 			{
-				DebugLog("无法解析登录页地址！");
+				DebugLog("无法解析[mengjiangwushuang/?origin=1407]");
 			}
 			string strLoginURL = m.Groups[1].Value;
 			result = PageQuery("weibo.com", strLoginURL, null);
@@ -58,7 +58,41 @@ namespace MJTool
 				+ CreateQueryString(data);
 			result = PageQuery("login.sina.com.cn", strSvrTimeURL, null);
 			
+			string strSvrTime, pcid, nonce, pubkey, rsakv;
+			m = Regex.Match(result, "\"servertime\":(.*?),\"pcid\":\"(.*?)\",\"nonce\":\"(.*?)\"," +
+			                "\"pubkey\":\"(.*?)\",\"rsakv\":\"(.*?)\"", RegexOptions.Singleline);
+			if (!m.Success)
+			{
+				DebugLog("无法解析[sso/prelogin.php]");
+			}
+			strSvrTime = m.Groups[1].Value;
+			pcid = m.Groups[2].Value;
+			nonce = m.Groups[3].Value;
+			pubkey = m.Groups[4].Value;
+			rsakv = m.Groups[5].Value;
 			
+			Random r = new Random(UnixTimeStamp(DateTime.Now));
+			data.Clear();
+			data.Add("entry", "weibo");
+			data.Add("gateway", "1");
+			data.Add("from", "");
+			data.Add("savestate", "7");
+			data.Add("useticket", "1");
+			data.Add("vsnf", "1");
+			data.Add("ssosimplelogin", "1");
+			data.Add("su", base64_encode(strUserName));
+			data.Add("service", "miniblog");
+			data.Add("servertime", strSvrTime);
+			data.Add("nonce", nonce);
+			data.Add("pwencode", "rsa2");
+			data.Add("rsakv", rsakv);
+			data.Add(sp, PswRSAEncode(strPassword, pubkey));
+			data.Add("encoding", "UTF-8");
+			data.Add("prelt", r.Next(100, 150).ToString());
+			data.Add("url", "http://ajaxlogin.php?framelogin=1&callback=parent.sinaSSOController.feedBackUrlCallBack");
+			data.Add("returntype", "META");
+			
+			result = PageQuery("login.sina.com.cn", "sso/login.php?client=ssologin.js(v1.4.2)", data);
 		}
 		
 		public static string sKey = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=";
@@ -75,6 +109,11 @@ namespace MJTool
 	        Encoding Ansi = Encoding.GetEncoding("GB2312");
 	        string wa = Ansi.GetString(sa);
 	        return wa;
+		}
+		
+		public static string PswRSAEncode(strPassword, pubkey)
+		{
+			return strPassword;
 		}
 		
 		public static string CreateQueryString(Dictionary<string, string> Data)
@@ -94,7 +133,7 @@ namespace MJTool
 		
 		private long UnixTimeStamp(DateTime time)
 		{
-			return Convert.ToInt64((DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds);
+			return Convert.ToInt64(time.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalMilliseconds);
 		}
 		
 		public bool ParseLogin(string strQueryResult)
